fdd1ed8daa708d41e136a2762ea1ff0b7d78ad1c
vamsi-devops <60419553+vamsi-devops@users.noreply.github.com>
2020-04-02 13:37:14 +0530

    Update starttomcat.sh

--- scripts/starttomcat.sh
+++ scripts/starttomcat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo service tomcat9 start
-sudo service tomcat9 restart
-sudo -i
+service tomcat9 start
+service tomcat9 restart
+touch f1
 cd /root/arachni-1.5.1-0.5.12-linux-x86_64/bin
 touch d1.html
 ./arachni http://http://18.223.114.39:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* --report-save-path=1.afr
