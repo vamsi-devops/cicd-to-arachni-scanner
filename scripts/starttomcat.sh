0ad151206323fd6483f990f7ff2e54a089eb1ecf
vamsi-devops <60419553+vamsi-devops@users.noreply.github.com>
2020-04-10 13:45:22 +0530

    Update starttomcat.sh

--- scripts/starttomcat.sh
+++ scripts/starttomcat.sh
@@ -13,7 +13,7 @@ cd /root/arachni-1.5.1-0.5.12/bin
 #./arachni http://13.234.98.68:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* --report-save-path=1.afr
 #./arachni_reporter 1.afr --reporter=html:outfile=my_report_new1.html.zip
 #aws s3 cp r3.html s3://arachni2/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-./arachni http://13.234.98.68:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* --report-save-path=3.afr
+./arachni http://13.234.98.68:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* --report-save-path=4.afr
 ./arachni_reporter 4.afr --reporter=html:outfile=my_report1.html.zip
 mkdir repo1
 unzip my_report1.html.zip -d repo1
