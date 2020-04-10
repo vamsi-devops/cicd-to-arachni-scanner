8fee3223a3e0a081d808593715140dd8d4d3c31c
vamsi-devops <60419553+vamsi-devops@users.noreply.github.com>
2020-04-10 13:39:25 +0530

    Update starttomcat.sh

--- scripts/starttomcat.sh
+++ scripts/starttomcat.sh
@@ -8,8 +8,17 @@ service tomcat9 restart
 touch f1
 #cd /root/arachni-1.5.1-0.5.12-linux-x86_64/bin
 cd /root/arachni-1.5.1-0.5.12/bin
-touch r3.html
-./arachni http://13.234.98.68:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* >r3.html
+#touch r3.html
+#./arachni http://13.234.98.68:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* >r3.html
 #./arachni http://13.234.98.68:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* --report-save-path=1.afr
 #./arachni_reporter 1.afr --reporter=html:outfile=my_report_new1.html.zip
-aws s3 cp r3.html s3://arachni2/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+#aws s3 cp r3.html s3://arachni2/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+./arachni http://13.234.98.68:8080/DemoPipelineJava-0.0.1-SNAPSHOT/ --checks=xss* --report-save-path=3.afr
+./arachni_reporter 4.afr --reporter=html:outfile=my_report1.html.zip
+mkdir repo1
+unzip my_report1.html.zip -d repo1
+cd repo1
+mv index.html report1.html
+aws s3 cp report1.html s3://arachni2/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+ 
+
